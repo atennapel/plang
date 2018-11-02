6197e692e44177cbea441ad17ffdec2469e8b052
Albert ten Napel <aptennap@gmail.com>
2018-11-02 15:43:18 +0100

    work

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log(10);
+import {
+  Kind,
+  kvar,
+  kfuns,
+} from './kinds';
+
+
+const type = kvar('type');
+console.log(
+  '' + kfuns(type, type, kfuns(type, type, type), type)
+);
---
+++ src/kinds.ts
@@ -0,0 +1,34 @@
+export abstract class Kind {
+
+  abstract toString(): string;
+
+}
+
+export class KVar extends Kind {
+
+  constructor(
+    readonly name: string,
+  ) { super() }
+
+  toString() {
+    return this.name;
+  }
+
+}
+export const kvar = (name: string) => new KVar(name);
+
+export class KFun extends Kind {
+
+  constructor(
+    readonly left: Kind,
+    readonly right: Kind,
+  ) { super() }
+
+  toString() {
+    return `(${this.left} -> ${this.right})`;
+  }
+
+}
+export const kfun = (left: Kind, right: Kind) => new KFun(left, right);
+export const kfunFrom = (ks: Kind[]) => ks.reduceRight((x, y) => kfun(y, x));
+export function kfuns(...ks: Kind[]) { return kfunFrom(ks) }
