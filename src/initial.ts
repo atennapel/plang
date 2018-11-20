2fede7679264a0334c49554c573f66c36351e093
Albert ten Napel <aptennap@gmail.com>
2018-11-20 20:51:29 +0100

    another approach to effect arrows

--- src/initial.ts
+++ src/initial.ts
@@ -14,6 +14,9 @@ export const kRow = kvar(nRow);
 export const nFun = name('->');
 export const tFun = tvar(nFun);
 
+export const nComp = name('Comp');
+export const kComp = kvar(nComp);
+
 export const tfun = (left: TypeN, right: TypeN) => tapps(tFun, left, right);
 export const tfunFrom = (ts: TypeN[]) => ts.reduceRight((x, y) => tfun(y, x));
 export function tfuns(...ts: TypeN[]) { return tfunFrom(ts) }
@@ -22,16 +25,18 @@ export const matchTFun = (type: TypeN): { left: TypeN, right: TypeN } | null =>
     { left: type.left.right, right: type.right } :
     null;
 
+export const tComp = tvar(nComp);
+export const tcomp = (type: TypeN, eff: TypeN) => tapps(tComp, type, eff);
+
 export const nFunE = name('FnEff');
 export const tFunE = tvar(nFunE);
 
-export const tfunEff = (left: TypeN, eff: TypeN, right: TypeN) => tapps(tFunE, left, eff, right);
-export const tfunE = (left: TypeN, right: TypeN) => tapps(tFunE, left, trowempty(), right);
+export const tfunE = (left: TypeN, right: TypeN) => tapps(tFunE, left, right);
 export const tfunEFrom = (ts: TypeN[]) => ts.reduceRight((x, y) => tfunE(y, x));
 export function tfunEs(...ts: TypeN[]) { return tfunEFrom(ts) }
-export const matchTFunE = (type: TypeN): { left: TypeN, eff: TypeN, right: TypeN } | null =>
-  isTApp(type) && isTApp(type.left) && isTApp(type.left.left) && isTVar(type.left.left.left) && type.left.left.left.name.equals(nFunE) ?
-    { left: type.left.left.right, eff: type.left.right, right: type.right } :
+export const matchTFunE = (type: TypeN): { left: TypeN, right: TypeN } | null =>
+  isTApp(type) && isTApp(type.left) && isTVar(type.left.left) && type.left.left.name.equals(nFunE) ?
+    { left: type.left.right, right: type.right } :
     null;
 
 export const nRec = name('Rec');
@@ -51,9 +56,12 @@ export const matchTVariant = (type: TypeN): TypeN | null =>
 export const initialContext = Context.of<Elem<NameRep>>(
   ckvar(nType),
   ckvar(nRow),
+  ckvar(nComp),
 
   ctvar(nFun, kfuns(kType, kType, kType)),
-  ctvar(nFunE, kfuns(kType, kRow, kType, kType)),
+
+  ctvar(nComp, kfuns(kType, kRow, kComp)),
+  ctvar(nFunE, kfuns(kType, kComp, kType)),
 
   ctvar(nRec, kfun(kRow, kType)),
   ctvar(nVar, kfun(kRow, kType)),
