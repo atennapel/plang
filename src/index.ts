bea2f26368f3e12e8250f3fdfbe7a8055a3460bb
Albert ten Napel <aptennap@gmail.com>
2018-12-07 16:27:49 +0100

    add todo

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,20 @@ import { abs, Var, app, showExpr } from "./exprs";
 import { showForall, TVar, Forall, tfun, tapp, TFun, teffs } from "./types";
 import { kType, kfun, kEff, kEffs } from "./kinds";
 
+/*
+TODO:
+  - handlers
+  - polymorphic effects
+  - polymorphic operations
+  - row polymorphic records/variants
+  - type recursion
+  - adts
+
+Bugs:
+    - app(v('fix'), abs(['rec', 'f'], app(v('caseList'), v('Nil'), abs(['h', 't'], app(v('Cons'), app(v('f'), v('h')), app(v('rec'), v('f'), v('t')))))))
+      effects dissapear ^
+*/
+
 const v = Var;
 const tv = TVar;
 
@@ -41,10 +55,7 @@ const ctx = extendContextMut(initial,
     caseList: Forall([['t', kType], ['r', kType], ['e', kEffs]], tfun(tv('r'), tfun(tv('t'), TFun(tapp(tv('List'), tv('t')), tv('e'), tv('r'))), TFun(tapp(tv('List'), tv('t')), tv('e'), tv('r')))),
   },
 );
-const expr = app(v('fix'),
-  abs(['rec', 'f'], app(v('caseList'),
-    v('Nil'), abs(['h', 't'], app(v('Cons'), app(v('f'), v('h')), app(v('rec'), v('f'), v('t'))))))
-);
+const expr = app(v('fix'), abs(['rec', 'f'], app(v('caseList'), v('Nil'), abs(['h', 't'], app(v('Cons'), app(v('f'), v('h')), app(v('rec'), v('f'), v('t')))))));
 console.log(`${showExpr(expr)}`);
 let time = Date.now();
 const res = throwEither(inferGen(ctx, expr, true));
