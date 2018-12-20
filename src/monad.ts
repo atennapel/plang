dc2a4fedfe83780d9d11c21459e3b7a4d36e2684
Albert ten Napel <aptennap@gmail.com>
2018-12-20 10:02:28 +0100

    clean slate

--- src/monad.ts
+++
@@ -1,6 +0,0 @@
-import { Either, Right } from "./either";
-
-export type Err = string;
-export type TC<T> = Either<Err, T>;
-
-export const ok: TC<true> = Right(true as true);
--- src/names.ts
+++
@@ -1,6 +0,0 @@
-export type Name = string;
-
-let id = 0;
-export const resetId = () => { id = 0 };
-export const fresh = (name: Name = '_') => `${name.split('$')[0]}\$${id++}`;
-export const namePart = (name: Name) => name.split('$')[0];
