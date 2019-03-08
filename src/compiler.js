1006abd9322588b4a5b5d4871428be8228645174
Albert ten Napel <aptennap@gmail.com>
2019-03-08 15:00:02 +0100

    add Con and Decon exprs

--- src/compiler.js
+++ src/compiler.js
@@ -2,6 +2,8 @@ const compile = e => {
   if (e.tag === 'Var') return e.name;
   if (e.tag === 'Abs') return `(${e.name} => ${compile(e.body)})`;
   if (e.tag === 'App') return `${compile(e.left)}(${compile(e.right)})`;
+  if (e.tag === 'Con') return compile(e.arg);
+  if (e.tag === 'Decon') return `(${e.name} => ${compile(e.body)})`;
 };
 
 module.exports = {
--- src/parser.js
+++ src/parser.js
@@ -2,6 +2,8 @@ const {
   Var,
   Abs,
   App,
+  Con,
+  Decon,
 } = require('./exprs');
 
 const err = msg => { throw new SyntaxError(msg) };
