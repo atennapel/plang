e58bd056978119a44c090a348533b63a327cc279
Albert ten Napel <aptennap@gmail.com>
2019-03-21 22:34:00 +0100

    start on PTIFART

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  showKinds: false,
-  logging: false,
-};
-
-export const log = (msg: string) => {
-  if (config.logging) console.log(msg);
-};
--- src/error.ts
+++
@@ -1,7 +0,0 @@
-export class InferError extends TypeError {
-  constructor(msg: string) { super(msg) }
-}
-
-export const infererr = (msg: string) => {
-  throw new InferError(msg);
-};
