a9c085cab428e1890adac2d6c3d30f3cf7e54c02
Albert ten Napel <aptennap@gmail.com>
2019-04-26 21:12:02 +0200

    some testing with machines

---
+++ hs/test2.js
@@ -0,0 +1,151 @@
+const VAR = 0;
+const ABS = 1;
+const APP = 2;
+const EXEC = 3;
+const Var = ix => ({ tag: VAR, ix });
+const Abs = body => ({ tag: ABS, body });
+const App = (left, right) => ({ tag: APP, left, right });
+function app() { return Array.from(arguments).reduce(App) }
+const Exec = (name, fn, body) => ({ tag: EXEC, name, fn, body });
+
+const showTerm = term => {
+  if (term.tag === VAR) return `${term.ix}`;
+  if (term.tag === ABS) return `(\\${showTerm(term.body)})`;
+  if (term.tag === APP) return `(${showTerm(term.left)} ${showTerm(term.right)})`;
+  if (term.tag === EXEC) return `(exec ${term.name} ${showTerm(term.body)})`;
+};
+
+const NIL = 0;
+const CONS = 1;
+const Nil = { tag: NIL };
+const Cons = (head, tail) => ({ tag: CONS, head, tail });
+const lookup = (env, ix) => {
+  while (env.tag === CONS) {
+    if (ix-- === 0) return env.head;
+    env = env.tail;
+  }
+  return null;
+};
+
+const showEnv = list => {
+  const r = [];
+  while (list.tag === CONS) {
+    r.push(showClos(list.head));
+    list = list.tail;
+  }
+  return `[${r.join(', ')}]`;
+};
+
+const free = (term, fr = []) => {
+  if (term.tag === VAR) {
+    const ix = term.ix;
+    if (fr.indexOf(ix) < 0) fr.push(ix);
+    return ix;
+  }
+  if (term.tag === ABS) {
+    
+  }
+  if (term.tag === APP) {
+    const a = free(term.left, fr);
+    const b = free(term.right, fr);
+    return Math.max(a, b);
+  }
+  if (term.tag === EXEC) return free(term.body, fr);
+};
+
+const Clos = (abs, env) => ({ abs, env });
+
+const showClos = clos => `{${showTerm(clos.abs)}@${showEnv(clos.env)}}`;
+
+const TOP = 0;
+const ARG = 1;
+const FUN = 2;
+const Top = { tag: TOP };
+const Arg = (term, env, rest) => ({ tag: ARG, term, env, rest });
+const Fun = (body, env, rest) => ({ tag: FUN, body, env, rest });
+
+const showCont = cont => {
+  if (cont.tag === TOP) return 'Top';
+  if (cont.tag === ARG) return `Arg(${showTerm(cont.term)}, ${showEnv(cont.env)}):${showCont(cont.rest)}`;
+  if (cont.tag === FUN) return `Fun(${showTerm(cont.body)}, ${showEnv(cont.env)}):${showCont(cont.rest)}`;
+};
+
+const State = (term, env, cont) => ({ term, env, cont });
+
+const showState = st => `(${showTerm(st.term)}, ${showEnv(st.env)}, ${showCont(st.cont)})`;
+
+const step = state => {
+  const { term, env, cont } = state;
+  if (term.tag === VAR) {
+    const v = lookup(env, term.ix);
+    if (!v) return false;
+    state.term = v.abs;
+    state.env = v.env;
+    return true;
+  }
+  if (term.tag === APP) {
+    state.term = term.left;
+    state.cont = Arg(term.right, env, cont);
+    return true;
+  }
+  if (term.tag === EXEC) {
+    term.fn();
+    state.term = term.body;
+    return true;
+  }
+  if (cont.tag === ARG) {
+    state.term = cont.term;
+    state.env = cont.env;
+    state.cont = Fun(term.body, env, cont.rest);
+    return true;
+  }
+  if (cont.tag === FUN) {
+    state.term = cont.body;
+    state.env = Cons(Clos(term, env), cont.env);
+    state.cont = cont.rest;
+    return true;
+  }
+  return false;
+};
+const steps = state => {
+  let i = 0;
+  console.log(i, showTerm(state.term), state.cont.tag);
+  while (step(state)) {
+    i++;
+    console.log(i, showTerm(state.term), state.cont.tag);
+  }
+  return i;
+};
+const initial = term => State(term, Nil, Top);
+
+const tI = Abs(Var(0));
+const tz = Abs(Abs(Var(0)));
+const ts = Abs(Abs(Abs(App(Var(1), app(Var(2), Var(1), Var(0))))));
+
+const tnil = Abs(Abs(Var(0)));
+const tcons = Abs(Abs(Abs(Abs(app(Var(1), Var(3), app(Var(2), Var(1), Var(0)))))));
+
+const reifyList = l => {
+  const r = [];
+  let i = 0;
+  const term = app(
+    l,
+    Abs(Abs(
+      app(
+        Abs(Exec('push', () => r.push(i), Var(0))),
+        app(
+          Var(1),
+          Abs(Exec('inc', () => i++, Var(0))),
+          Exec('reset', () => i = 0, Var(0)))))), tI);
+  const state = initial(term);
+  steps(state);
+  return r.reverse();
+};
+
+const n = app(ts, app(ts, tz));
+const l = app(tcons, tz, app(tcons, app(ts, tz), app(tcons, n, tnil)));
+let i = 0;
+const arr = app(app(ts, app(ts, app(ts, tz))), Abs(Exec('inc', () => i++, Var(0))), tI);
+steps(initial(arr));
+console.log(i);
+
